97b9a1693aca21ed794532fc9d57c9ed7f87aac3
Amelija <A250138AL@rvt.lv>
2026-06-01 20:31:43 +0300

    Samainiju kodu

--- clicker.js
+++ clicker.js
@@ -4,61 +4,71 @@ let skinLevel = 0;
 let bgLevel = 0;
 let upgradePrice = 30;
 
-const scoreText = document.getElementById("score");
-const doge = document.getElementById("doge");
-const infoBox = document.getElementById("infoBox");
+let scoreText = document.getElementById("score");
+let doge = document.getElementById("doge");
+let infoBox = document.getElementById("infoBox");
+let endBox = document.getElementById("endBox");
 
 const skins = [
-    "https://pngimg.com/uploads/doge_meme/doge_meme_PNG8.png",
-    "https://m.media-amazon.com/images/I/51tlqVzLmvL._AC_UF894,1000_QL80_.jpg",
-    "https://ih1.redbubble.net/image.4994245202.0402/bg,f8f8f8-flat,750x,075,f-pad,750x1000,f8f8f8.jpg",
-    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQF5WP4LyqKySclp_Y-cIEs0bbgDBQVGqBekeYRaPvt0Q&s",
-    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTMT0T5URvpF5-ImbdM61kZtQpPYFHrnoPWLGm0LrNnVQ&s",
-    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTzXQ97rgzc-aEQicAS_F6iUDzDiS57efWTtD-uzKrcvw&s",
-    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ6hbtLApug53zDL28kA7mYBclBXqlodwIh0MYAc5LGTg&s",
-    "https://pngimg.com/uploads/doge_meme/doge_meme_PNG9.png",
-    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTNBocUO59F98YDbLqL0ktSmjag5whp2ULAwQ&s",
-    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSR-HSdTFqDfpClDdmFddA_ZKx2PAjpkrgEBf__RYHA5Q&s",
-    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQZtp676iuHxFkY7aWELA4PfUKea2EIU4nQ5w&s",
-    "https://media3.giphy.com/media/v1.Y2lkPTZjMDliOTUyaTlybG5jcDlpdmJ6cjZwczljcXg1bW80Y3FxYjY4M2hiY24weHN0eSZlcD12MV9zdGlja2Vyc19zZWFyY2gmY3Q9cw/vS8deMiryn69PFGwJQ/200w_s.gif",
-    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTzPlC2XgGo3HWxtgie6a-X7kNot2O1PSmq3A&s"
+"https://pngimg.com/uploads/doge_meme/doge_meme_PNG8.png",
+"https://m.media-amazon.com/images/I/51tlqVzLmvL._AC_UF894,1000_QL80_.jpg",
+"https://ih1.redbubble.net/image.4994245202.0402/bg,f8f8f8-flat,750x,075,f-pad,750x1000,f8f8f8.jpg",
+"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQF5WP4LyqKySclp_Y-cIEs0bbgDBQVGqBekeYRaPvt0Q&s",
+"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTMT0T5URvpF5-ImbdM61kZtQpPYFHrnoPWLGm0LrNnVQ&s",
+"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTzXQ97rgzc-aEQicAS_F6iUDzDiS57efWTtD-uzKrcvw&s",
+"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ6hbtLApug53zDL28kA7mYBclBXqlodwIh0MYAc5LGTg&s",
+"https://pngimg.com/uploads/doge_meme/doge_meme_PNG9.png",
+"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTNBocUO59F98YDbLqL0ktSmjag5whp2ULAwQ&s",
+"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSR-HSdTFqDfpClDdmFddA_ZKx2PAjpkrgEBf__RYHA5Q&s",
+"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQZtp676iuHxFkY7aWELA4PfUKea2EIU4nQ5w&s",
+"https://media3.giphy.com/media/v1.Y2lkPTZjMDliOTUyaTlybG5jcDlpdmJ6cjZwczljcXg1bW80Y3FxYjY4M2hiY24weHN0eSZlcD12MV9zdGlja2Vyc19zZWFyY2gmY3Q9cw/vS8deMiryn69PFGwJQ/200w_s.gif",
+"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTzPlC2XgGo3HWxtgie6a-X7kNot2O1PSmq3A&s"
 ];
 
-const skinPrices = [0, 50, 100, 150, 250, 350, 450, 550, 650, 750, 850, 950, 1150, 1350];
-const backgrounds = ["https://images.goway.com/production/styles/article_featured_image_2xl/s3/featured_images/japan_tokyo_akihabara_AdobeStock_295310062_Editorial_Use_Only.jpg.webp?VersionId=QsALmYD2qaEJEwTHHBOtnO3bgPlu56hp&h=43fc81ba&itok=ZRsG7bGh",
+const skinPrices = [0,50,100,150,250,350,450,550,650,750,850,950,1050,1200];
 
-    "https://www.pixelstalk.net/wp-content/uploads/2016/06/Free-Desktop-1600x900-Wallpapers-Download.jpg",
-
-    "https://fons.grizly.club/uploads/posts/2025-06/04/17490291445631.jpg",
-
-    "https://wallpapers.com/images/hd/street-with-red-sunset-dee8h6e9s4r3ylay.jpg",
-
-    "https://wallpapercave.com/wp/wp3987071.jpg"
+const backgrounds = [
+"https://images.goway.com/production/styles/article_featured_image_2xl/s3/featured_images/japan_tokyo_akihabara_AdobeStock_295310062_Editorial_Use_Only.jpg.webp",
+"https://www.pixelstalk.net/wp-content/uploads/2016/06/Free-Desktop-1600x900-Wallpapers-Download.jpg",
+"https://fons.grizly.club/uploads/posts/2025-06/04/17490291445631.jpg",
+"https://wallpapers.com/images/hd/street-with-red-sunset-dee8h6e9s4r3ylay.jpg",
+"https://wallpapercave.com/wp/wp3987071.jpg"
 ];
 
-doge.onclick = function () {
+function clickDoge() {
     score += clickPower;
-    scoreText.textContent = score;
+    scoreText.innerHTML = score;
+
+    doge.style.transform = "scale(0.9)";
+    setTimeout(function() {
+        doge.style.transform = "scale(1)";
+    }, 100);
+
+    if (score >= 5000) {
+        endGame();
+    }
 
     if (skinLevel + 1 < skinPrices.length &&
         score >= skinPrices[skinLevel + 1]) {
-        alert("Tu vari nopirkt jaunu skinu!");
+        alert("Tu vari nopirkt jaunu skinu! ;D ");
     }
-};
+}
 
 function buySkin() {
     let price = skinPrices[skinLevel + 1];
 
-    if (!price) {
-        alert("Visi skini nopirkti!");
-    } else if (score >= price) {
+    if (price == undefined) {
+        alert("Visi skini nopirkti! :)) ");
+    }
+    else if (score >= price) {
         score -= price;
         skinLevel++;
         doge.src = skins[skinLevel];
-        scoreText.textContent = score;
-        alert("Jauns skins nopirkts!");
-    } else {
-        alert("Vēl vajag " + (price - score) + " klikšķus");
+        scoreText.innerHTML = score;
+        alert("Jauns skins nopirkts! =^-^= ");
+    }
+    else {
+        alert("Vajag vēl " + (price - score));
     }
 }
 
@@ -67,15 +77,15 @@ function upgradeClicks() {
         score -= upgradePrice;
         clickPower++;
         upgradePrice += 30;
-        scoreText.textContent = score;
-        alert("Klikšķa spēks: " + clickPower);
-    } else {
+        scoreText.innerHTML = score;
+        alert("~Klikšķa spēks: " + clickPower);
+    }
+    else {
         alert("Vajag vēl " + (upgradePrice - score));
     }
 }
 
 function changeBackground() {
-
     bgLevel++;
 
     if (bgLevel >= backgrounds.length) {
@@ -83,7 +93,7 @@ function changeBackground() {
     }
 
     document.body.style.backgroundImage =
-        `url('${backgrounds[bgLevel]}')`;
+        "url('" + backgrounds[bgLevel] + "')";
 }
 
 function showInfo() {
@@ -92,10 +102,28 @@ function showInfo() {
         : "Visi atbloķēti";
 
     infoBox.style.display = "block";
-    infoBox.innerHTML = `
-        Klikšķi: ${score}<br>
-        Klikšķa spēks: ${clickPower}<br>
-        Līdz skinam: ${next}<br>
-        Upgrade cena: ${upgradePrice}
-    `;
+
+    infoBox.innerHTML =
+        "Klikšķi: " + score + "<br>" +
+        "Klikšķa spēks: " + clickPower + "<br>" +
+        "Līdz skinam: " + next + "<br>" +
+        "Upgrade cena: " + upgradePrice;
+}
+
+function endGame() {
+    endBox.style.display = "block";
+    doge.style.display = "none";
+}
+
+function restartGame() {
+    score = 0;
+    clickPower = 1;
+    skinLevel = 0;
+    bgLevel = 0;
+    upgradePrice = 30;
+
+    scoreText.innerHTML = score;
+    doge.src = skins[0];
+    doge.style.display = "block";
+    endBox.style.display = "none";
 }
